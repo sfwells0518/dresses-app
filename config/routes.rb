88823d54c2f4ad5272a3adc2a0b1cae28bafb53c
Sam Wells <sfwells0518@gmail.com>
2023-04-01 17:02:42 -0500

    add update REST to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "/dresses" => "dresses#index"
   get "/dresses/:id" => "dresses#show"
   post "/dresses" => "dresses#create"
-  # patch "/dresses/:id" => "dresses#update"
+  patch "/dresses/:id" => "dresses#update"
   # delete "/dresses/:id" => "dresses#destroy"
 end
