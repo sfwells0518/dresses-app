742c3a26d4e83605401f5a6c1dde99d38f49c984
Sam Wells <sfwells0518@gmail.com>
2023-04-01 16:54:27 -0500

    add show REST route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/dresses" => "dresses#index"
-  # get "/dresses/:id" => "dresses#show"
+  get "/dresses/:id" => "dresses#show"
   # post "/dresses" => "dresses#create"
   # patch "/dresses/:id" => "dresses#update"
   # delete "/dresses/:id" => "dresses#destroy"
